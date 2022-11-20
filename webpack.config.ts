e64bcf3aceef791b378c7bd7c83a1c33f2dcfd1a
blackmoja <limlim980625@gmail.com>
2022-11-20 19:21:23 +0900

    feat: SG Design System 웹펙과 설정 동기화 진행

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,24 +1,32 @@
-import { resolve } from 'path';
+import { ProvidePlugin } from 'webpack';
+import path from 'path';
+import packageJson from './package.json';
 import type { Configuration } from 'webpack';
 
+const resolve = (_path: string) => path.resolve(__dirname, _path);
+
 const config: Configuration = {
-  context: __dirname,
-   entry: './src/components/index.ts',
-  name: 'webpack-example',
+  entry: './src/index.ts',
   output: {
-    path: resolve(__dirname, 'dist'),
+    path: resolve('../dist'),
     filename: 'index.js',
+    umdNamedDefine: true,
     library: {
-      type: 'module',
+      name: packageJson.name,
+      type: 'umd',
     },
     clean: true,
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+    },
   },
   resolve: {
     modules: [resolve('../src'), 'node_modules'],
     extensions: ['.ts', '.tsx', '.js', '.css'],
-  },
-  experiments: {
-    outputModule: true,
   },
   module: {
     rules: [
@@ -47,6 +55,11 @@ const config: Configuration = {
       },
     ],
   },
+  plugins: [
+    new ProvidePlugin({
+      React: 'react',
+    }),
+  ],
 };
 
 export default config;
